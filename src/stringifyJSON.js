23e5df2358260a253df10ae581b1e03bf8b2fd2b
Bao Nguyen <baoducng@gmail.com>
2014-04-24 11:25:00 -0700

    first version

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,40 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+    var result, temp;
+    //use typeof
+    //if object
+    if (typeof obj === "object"){
+        result = '{';
+        //process that property in the object, and then remove that property?
+        //use splice to insert into the middle of the string.
+        //delete the object
+        for (var key in obj){
+            result.concat('"' + key + '":');
+            result.concat(stringifyJSON(obj[key]));
+        }
+        result.concat('}');
+        return result;
+        //write an function to check if object is null
+    }
+    //if array
+    else if (typeof obj === "array"){
+        result = '[';
+        for (var i =0; i < obj.length; i++){
+            //feed the function back in here
+            result.concat(stringifyJSON(obj[i]))
+        }
+        result.concat(']');
+        return result;
+    }
+
+    //if number, string, undefined => easy
+    else if (typeof obj === "number"){
+        temp = "'" + obj + "'";
+        return temp;
+    }
+    else {// (typeof obj === "string" || typeof obj === "boolean" || typeof obj === "undefined"){
+        temp = (typeof obj);
+        return temp;
+    }
 };
