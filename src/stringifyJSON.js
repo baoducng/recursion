5fc30d54a616703f7b2ca3bd84abff849ea72ef9
Bao Nguyen <baoducng@gmail.com>
2014-04-24 12:07:07 -0700

    stuck at [8]

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,39 +4,58 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
     var result, temp;
-    //use typeof
-    //if object
-    if (typeof obj === "object"){
-        result = '{';
-        //process that property in the object, and then remove that property?
-        //use splice to insert into the middle of the string.
-        //delete the object
-        for (var key in obj){
-            result.concat('"' + key + '":');
-            result.concat(stringifyJSON(obj[key]));
+    //if array
+    if (Array.isArray(obj)){
+        if (obj.length === 0){return '[]'}
+        else{
+            result = '[';
+            for (var i =0; i < obj.length; i++){
+                //feed the function back in here
+                result.concat(stringifyJSON(obj[i]))
+                }
+            result.concat(']');
+            return result;
         }
+    }
+
+    //if object
+    else if (typeof obj === "object"){
+        //write an function to check if object is null
+        if (!obj){
+            return 'null'
+        }
+        else{
+
+            result = '{';
+            //process that property in the object, and then remove that property?
+            //use splice to insert into the middle of the string.
+            //delete the object
+            for (var key in obj){
+                result.concat('"' + key + '":');
+                result.concat(stringifyJSON(obj[key]));
+            }
         result.concat('}');
         return result;
-        //write an function to check if object is null
-    }
-    //if array
-    else if (typeof obj === "array"){
-        result = '[';
-        for (var i =0; i < obj.length; i++){
-            //feed the function back in here
-            result.concat(stringifyJSON(obj[i]))
         }
-        result.concat(']');
-        return result;
     }
 
     //if number, string, undefined => easy
-    else if (typeof obj === "number"){
-        temp = "'" + obj + "'";
+    else if (typeof obj === "number" || typeof obj === "boolean"){
+        temp = obj.toString();
         return temp;
     }
-    else {// (typeof obj === "string" || typeof obj === "boolean" || typeof obj === "undefined"){
-        temp = (typeof obj);
+    /*
+    else if (typeof obj === "boolean"){
+        temp = obj.toString();
+        return temp;
+    }
+    */
+    else if (typeof obj === "string"|| typeof obj === "undefined"){
+        temp = '"' + obj + '"';
         return temp;
     }
 };
+
+var test1 = Array.isArray([]);
+var test = JSON.stringify('Hello World');
+console.log(test1);
