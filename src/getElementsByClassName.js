8c23790475798b9db499f07acda8a699678e9693
Hack Reactor Students <accounts@hackreactor.com>
2014-07-10 18:52:24 -0700

    formatting

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,48 +5,21 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-
   var result = [];
   var currentNode = document.body;
-  var storage;
-
-    function checkNode(node){
-        if (node.classList){
-            if(node.classList.contains(className)){result.push(node)}
-        }
-        if (node.hasChildNodes()){
-            for (var i =0; i < node.childNodes.length; i++){
-                checkNode(node.childNodes[i]);
-            }
-        }
-    }
-    checkNode(currentNode);
-
-    return result;
-};
-
-/*
-
- var getElementsByClassName = function(className){
-
- var result = [];
- var currentNode = document.body;
- var storage;
-
- function checkNode(node){
+  function checkNode(node){
     if (node.classList){
-        if(node.classList.contains(className)){result.push(node)}
-        if (node.hasChildNodes()){
-        storage = node.childNodes;
-        for (var i =0; i < storage.length; i++){
-        checkNode(storage[i]);
-        }
-       }
- }
- }
- checkNode(currentNode);
- return result;
- };
- */
+      if(node.classList.contains(className)){
+        result.push(node);
+      }
+    }
+    if (node.hasChildNodes()){
+      for (var i =0; i < node.childNodes.length; i++){
+        checkNode(node.childNodes[i]);
+      }
+    }
+  }
+  checkNode(currentNode);
 
-
+  return result;
+};
